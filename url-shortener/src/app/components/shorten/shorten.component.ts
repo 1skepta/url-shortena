24a954e4031fee39c11cb6359439ba02a619a3fd
1skepta <prospaul999@gmail.com>
2025-01-10 17:11:46 +0000

    add validations to url input section

--- url-shortener/src/app/components/shorten/shorten.component.ts
+++ url-shortener/src/app/components/shorten/shorten.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shorten',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './shorten.component.html',
-  styleUrl: './shorten.component.css'
+  styleUrl: './shorten.component.css',
 })
 export class ShortenComponent {
+  linkInput: string = '';
+  showError: boolean = false;
 
+  shortenLink() {
+    if (!this.linkInput) {
+      this.showError = true;
+    } else {
+      this.showError = false;
+    }
+  }
+
+  onInputChange() {
+    if (this.linkInput) {
+      this.showError = false;
+    }
+  }
 }

0b6600c06475e953250aae326b8eb901e91e7aad
1skepta <prospaul999@gmail.com>
2025-01-10 00:46:36 +0000

    design the hamburger page

--- url-shortener/src/app/app.component.ts
+++ url-shortener/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, BodyComponent],
+  imports: [HeaderComponent, BodyComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
--- url-shortener/src/app/components/header/header.component.ts
+++ url-shortener/src/app/components/header/header.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  onOptionClick(option: string): void {
+    console.log(`${option} clicked!`);
+  }
 }

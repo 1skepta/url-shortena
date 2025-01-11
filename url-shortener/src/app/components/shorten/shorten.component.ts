0701ad8b39e7a645aac0817bd7377a199391d12c
1skepta <prospaul999@gmail.com>
2025-01-11 23:43:44 +0000

    implement api to shorten url

--- url-shortener/src/app/components/shorten/shorten.component.ts
+++ url-shortener/src/app/components/shorten/shorten.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import axios from 'axios';
 
 @Component({
   selector: 'app-shorten',
@@ -11,12 +12,25 @@ import { FormsModule } from '@angular/forms';
 export class ShortenComponent {
   linkInput: string = '';
   showError: boolean = false;
+  results: Array<{ text: string; shortUrl: string; isCopied: boolean }> = [];
 
-  shortenLink() {
+  async shortenLink() {
     if (!this.linkInput) {
       this.showError = true;
-    } else {
-      this.showError = false;
+      return;
+    }
+    this.showError = false;
+    try {
+      const response = await axios.post(
+        'https://corsproxy.io/https://cleanuri.com/api/v1/shorten',
+        new URLSearchParams({ url: this.linkInput })
+      );
+      const shortUrl = response.data.result_url;
+
+      this.results.push({ text: this.linkInput, shortUrl, isCopied: false });
+      this.linkInput = '';
+    } catch (error) {
+      console.error('Error: ', error);
     }
   }
 
@@ -24,5 +38,14 @@ export class ShortenComponent {
     if (this.linkInput) {
       this.showError = false;
     }
+  }
+
+  copyToClipboard(index: number) {
+    navigator.clipboard.writeText(this.results[index].shortUrl);
+    this.results[index].isCopied = true;
+
+    setTimeout(() => {
+      this.results[index].isCopied = false;
+    }, 700);
   }
 }
